1d5e4ecd8cce2d49047091af47443355d13c043d
6pburt <67713261+6pburt@users.noreply.github.com>
2020-07-20 15:17:47 +1200

    Browse page
    Browse page was added with improved query and header image.

--- browse.php
+++ browse.php
@@ -1,58 +1,92 @@
-<!-- localhost/UseThis/03_UserListTable.php-->
+<!-- localhost/musicdb/index.php -->
 
 <!DOCTYPE html>
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>UserListTable</title>
-<link rel="stylesheet" type="text/css" href="style.css">
+	<meta charset="ISO-8859-1">
+	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+	<title>home</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 	<body>
-		<div id="container">
+		<div id="wrapper">
+		<?php require_once('nav.php'); ?>
 			<div id="header">
-				<h1>How to output the users using a table format</h1>
+				<img class="himg" src="images/headerimg.jpg">
 			</div><!-- end of header -->
 			<?php require_once('nav.php'); ?>
-			<div id="content">
-				<img src="Jellyfish.jpg" alt="Just another broken image link" title="An image for an images sake" width="560px" />
-				
+	
+			<div id="query">
+			<div id="row" class="titles">
+				<div class='output'>Song Name</div>
+				<div class='output'>Album Name</div>
+				<div class='output'>Duration</div>
+				<div class='output'>Artist(s)</div>
+				<div class='output'>Genre(s)</div>
+			</div>
 				<?php
 					//connect.php (tells where to connect servername, username, password, dbaseName)
 					require_once('connect.php');
-					print "<h3>Connected to server</h3>\n";
 					
-					$query=("SELECT * FROM login"); //create query
+					//create a variable to store sql code for the 'display all users' query
+					$query = ("SELECT s.id id, s.song_name song, ab.album album, s.duration duration
+					FROM song s
+					INNER JOIN album ab ON s.album_id = ab.ab_id
+					");
 					
-					$res=mysqli_query($con,$query); //run query
+					//run the query
+					$res = mysqli_query($con,$query);
 					
-					//starts the table
-					echo "<table>";	
-					//loops to give all queries data
-					echo "<tr>";//table row starts
-					echo "<td>";//table column 1 starts
-					print "<h3>user_id</h3>";
-					echo "</td>";
-					echo "<td>";//table column 2 starts 
-					print "<h3>password</h3>";
-					echo "</td>";					
-					while ($output=mysqli_fetch_array($res)) 
-					{//this infermation continues for the loop count
-					echo "<tr>";//table row starts
-					echo "<td>";//table column 1 starts
-					echo $output["user_id"];
-					echo "</td>";
-					echo "<td>";//table column 2 starts 
-					echo $output["password"];
-					echo "</td>";
-					echo "</tr>";//table row finishes
-					}// The loop finishes
-					echo "</table>";//table closes
-				?>
+					//run query results using a while loop
+						while ($output = mysqli_fetch_array($res))
+						{	
+							$dur = $output['duration'];
+							if(strlen(strval(($dur % 60))) == 2) {
+							$durval = (($dur - $dur % 60) / 60).":".strval(($dur % 60));}
+							elseif(strlen(strval(($dur % 60))) == 1) {
+								$durval = (($dur - $dur % 60) / 60).":0".strval(($dur % 60));}
+							elseif(strlen(strval(($dur % 60))) == 0) {
+								$durval = (($dur - $dur % 60) / 60).":00".strval(($dur % 60));}
+							
+							$query2 = ("SELECT g.genre genre
+							FROM genre g
+							INNER JOIN genre_link gl ON g.g_id = gl.genre_id
+							WHERE song_id = ".$output['id']);
 
+							$query3 = ("SELECT a.artist artist
+							FROM artist a
+							INNER JOIN artist_link al ON a.a_id = al.artist_id
+							WHERE song_id = ".$output['id']);
+
+							$res2 = mysqli_query($con,$query2);
+							$res3 = mysqli_query($con,$query3);
+
+							$genre = "";
+							$artist = "";
+							
+							while ($output2 = mysqli_fetch_array($res2))
+							{
+								$genre = $output2['genre']." ".$genre;
+							}
+
+							while ($output3 = mysqli_fetch_array($res3))
+							{
+								$artist = $output3['artist']." ".$artist;
+							}
+
+							echo("<div id='row'>"
+							."<div class='output'>".$output['song']."</div>"
+							."<div class='output'>".$output['album']."</div>"
+							."<div class='output'>".$durval."</div>"
+							."<div class='output'>".$artist."</div>"
+							."<div class='output'>".$genre."</div>"
+							."</div>");
+				//closes the while loop
+					}
+				?>	
 			</div><!-- end of content -->
 			<div id="footer">
-				<h2>The User List in a tabulated format</h2>
-				<p>Tawa</p>
+				<p>Copyright Pierce Burt 2020</p>
 			</div><!-- end of footer -->
 		</div><!-- end of container -->
 	</body>
--- nav.php
+++ nav.php
@@ -1,22 +1,22 @@
 <html>
     <nav>
-        <ul id = "navcontent">
-            <li><a href="index.php">Home</a></li>
-            <li><a href="browse.php">Browse</a></li>
-            <li><a href="genres.php">Genres</a></li>
+        <div id = "navcontent">
+            <a class='navlink' href="index.php">Home</a>
+            <a class='navlink' href="browse.php">Browse</a>
+            <a class='navlink' href="genres.php">Genres</a>
             <div class="dropdown">
-                <ul class="dropdown">
-                    <li>Account</li>
+                <p class="dropdownitem">Account</p>
+                <div class="dropdown1">
                     <?php
                         session_start();
                         if(!isset($_SESSION['login_user'])) {
-                            echo("<li><a href='login.php'>Login</a></li><li><a href='register.php'>Register</a></li>");
+                            echo("<a class='navlink' href='login.php'>Login</a><a class='navlink' href='register.php'>Register</a>");
                         }
                         else{
-                            echo("<li><a href='logout.php'>Log Out</a></li><li><a href='account.php'>Account</a></li>");
+                            echo("<a class='navlink' href='logout.php'>Log Out</a><a class='navlink' href='account.php'>Account</a>");
                         }
                     ?>
-                </ul>
+                </div>
             </div>
-        </ul>
+        </div>
     </nav>

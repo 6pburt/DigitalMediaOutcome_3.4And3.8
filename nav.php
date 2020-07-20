1b4a23ae821a0e97c046918c707335a944a0c073
6pburt <67713261+6pburt@users.noreply.github.com>
2020-07-20 12:06:30 +1200

    Update nav.php

---
+++ nav.php
@@ -0,0 +1,22 @@
+<html>
+    <nav>
+        <ul id = "navcontent">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="browse.php">Browse</a></li>
+            <li><a href="genres.php">Genres</a></li>
+            <div class="dropdown">
+                <ul class="dropdown">
+                    <li>Account</li>
+                    <?php
+                        session_start();
+                        if(!isset($_SESSION['login_user'])) {
+                            echo("<li><a href='login.php'>Login</a></li><li><a href='register.php'>Register</a></li>");
+                        }
+                        else{
+                            echo("<li><a href='logout.php'>Log Out</a></li><li><a href='account.php'>Account</a></li>");
+                        }
+                    ?>
+                </ul>
+            </div>
+        </ul>
+    </nav>
